cf89b5ab5cad930b91d2bbdf141373b5aabc7bf1
ykkTechc <102883569+ykkTechc@users.noreply.github.com>
2022-05-31 14:17:53 +0900

    sauna model  shop 追加

--- app/models/sauna.rb
+++ app/models/sauna.rb
@@ -1,6 +1,7 @@
 class Sauna < ApplicationRecord
   belongs_to :user
-
+  
+  validates :shop,      presence: true
   validates :hot,       presence: true
   validates :ice,       presence: true
   validates :chill,     presence: true
