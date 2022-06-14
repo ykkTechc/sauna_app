29b0ad2f23b545d259b0531703bd49f62243b6ea
ykkTechc <102883569+ykkTechc@users.noreply.github.com>
2022-06-14 20:55:12 +0900

    サウナ　テスト実装

--- spec/factories/saunas.rb
+++ spec/factories/saunas.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :sauna do
-    
+    shop   {'スパサウナ'}
+    hot    {'サウナ'}
+    ice    {'水風呂'}
+    chill  {'外気浴'}
+
+    association :user
   end
 end
--- spec/models/sauna_spec.rb
+++ spec/models/sauna_spec.rb
@@ -1,13 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Sauna, type: :model do
-  require 'rails_helper'
+
+  before do
+    @sauna = FactoryBot.build(:sauna)
+  end 
+
   describe 'サウナ投稿機能' do
-    it 'nicknameが空では登録できない' do
-      user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
-      user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+
+    context 'サ活投稿ができる場合' do
+      it 'すべての情報があれば登録できる' do
+        expect(@sauna).to be_valid
+      end
+     end
+     
+     it '店名が空では登録できない' do
+      sauna = FactoryBot.build(:sauna)
+      sauna.shop = ''
+      sauna.valid?
+      expect(sauna.errors.full_messages).to include "Shop can't be blank"
     end
+
+    it 'サウナが空では登録できない' do
+      sauna = FactoryBot.build(:sauna)
+      sauna.hot = ''
+      sauna.valid?
+      expect(sauna.errors.full_messages).to include "Hot can't be blank"
+    end
+
+    it '水風呂が空では登録できない' do
+      sauna = FactoryBot.build(:sauna)
+      sauna.ice = ''
+      sauna.valid?
+      expect(sauna.errors.full_messages).to include "Ice can't be blank"
+    end
+
+    it '外気浴が空では登録できない' do
+      sauna = FactoryBot.build(:sauna)
+      sauna.chill = ''
+      sauna.valid?
+      expect(sauna.errors.full_messages).to include "Chill can't be blank"
+    end
+
   end
-end
 end
