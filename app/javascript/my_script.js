08108c6c9a7ebfb6417f79c14f7afed2d9a434f3
ykkTechc <102883569+ykkTechc@users.noreply.github.com>
2022-06-15 23:06:47 +0900

    スライドショー　実装

--- app/javascript/my_script.js
+++
@@ -1,8 +0,0 @@
-$('#nav_btn .next').click(function(){
- $('#slide:not(:animate)').animate({
-   'margin-left' : -1*$('#slide li').width()
- },function(){
-   $('#slide').append($('#slide li:first-child'));
-   $('#slide').css('margin-left', '0');
-  });
-});
